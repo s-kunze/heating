a06958d3b94897dbc19af20b2e392a1d7587e25c
Stefan Kunze <skson@gmx.de>
2016-05-07 20:41:05 +0200

    Move Application.java

--- src/main/java/de/kunze/heating/client/Application.java
+++ src/main/java/de/kunze/heating/client/Application.java
@@ -1,15 +1,12 @@
+package de.kunze.heating.client;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class Application {
 
-    public static void main(String[] args) {
-
-        SpringApplication.run(Application.class, args);
-
-    }
-
-
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
 }
-
