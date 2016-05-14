7b24c169ce95c39bcd8821d3563877c8e47c08be
Stefan <skson@gmx.de>
2016-05-14 03:16:58 +0200

    add base spring boot + angularjs app

--- src/main/java/de/kunze/heating/client/Application.java
+++ src/main/java/de/kunze/heating/client/Application.java
@@ -2,6 +2,8 @@ package de.kunze.heating.client;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @SpringBootApplication
 public class Application {
@@ -9,4 +11,5 @@ public class Application {
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
 	}
+
 }
